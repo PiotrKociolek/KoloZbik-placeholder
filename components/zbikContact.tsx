0af886ed0a73a7f822be1786a97c1d969cd97c66
Piotr Kociołek <piotrkociolek2002@gmail.com>
2024-05-24 22:56:40 +0200

    placeholder changes v3

--- components/zbikContact.tsx
+++ components/zbikContact.tsx
@@ -9,11 +9,9 @@ const ZbikContact =()=>{
             <Card className="p-4">
                 <CardBody className="overflow-visible">
                     <p className="text-lg font-semibold mb-2">Adres:</p>
-                    <p className="text-gray-600 ">Ul. Przykładowa 123</p>
-                    <p className="text-lg font-semibold mt-4 mb-2">E-mail:</p>
-                    <p className="text-gray-600 ">przykladowy@email.com</p>
+                    <p className="text-gray-600 ">Nowy Sącz 33-300 ul. Lwowska 91</p>
                     <p className="text-lg font-semibold mt-4 mb-2">Konto bankowe:</p>
-                    <p className="text-gray-600 ">1234 5678 9012 3456 7890 1234</p>
+                    <p className="text-gray-600 ">64 8811 0006 0000 0000 0130 0001</p>
                     <p className="text-lg font-semibold mt-4 mb-2">Kontakt z Zarządem w sekcji zarząd:</p>
 
                     <a href="/management">
